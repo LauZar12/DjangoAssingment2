3068efd5ae61f01cc420a62bf7595f574b9a0ed5
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2024-03-02 10:44:34 -0500

    Add files via upload

--- movie/management/commands/add_movies_db.py
+++ movie/management/commands/add_movies_db.py
@@ -4,21 +4,19 @@ import os
 import json
 
 class Command(BaseCommand):
-    help = 'Load movies from movie_descriptions.json into de Movie model'
+    help = 'Load movies from movie_descriptions.json into the Movie model'
 
     def handle(self, *args, **kwargs):
         json_file_path = 'movie/management/commands/movies.json'
-
-        #To load de data from the JSON file
         with open(json_file_path, 'r') as file:
             movies = json.load(file)
 
-        #Add products to the database
         for i in range(100):
             movie = movies[i]
             exist = Movie.objects.filter(title = movie['title']).first()
             if not exist:
                 Movie.objects.create(title = movie['title'],
-                                     image = 'movie/images/default.jpg',
-                                     genre= movie['genre'],
+                                     img = 'movie/images/default.jpg',
+                                     genre = movie['genre'],
                                      year = movie['year'])
+                
