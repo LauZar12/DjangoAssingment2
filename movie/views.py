64732d7894da37217c690ed2bd2b8db560ab7eb1
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2024-03-04 18:57:19 -0500

    Add files via upload

--- movie/views.py
+++ movie/views.py
@@ -1,51 +1,59 @@
-<<<<<<< HEAD
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from .models import Movie
 
 import matplotlib.pyplot as plt
 import matplotlib
 import io
-import urllib, base64
+import base64
 
 # Create your views here.
+
 def home(request):
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm,'movies':movies})
+
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
-	return HttpResponse('<h1>Welcome to About Page</h1>')
+	return render(request, 'about.html')
 
 def signup(request):
-     email = request.GET.get('email')
-     return render(request, 'signup.html', {'email':email})
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
 
 def stadistics_view(request):
+
+    #===========graph 1===========
+
     matplotlib.use('Agg')
     years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
+
     movie_counts_by_year = {}
     for year in years:
-        if year:
-            movies_in_year = Movie.objects.filter(year=year)
+        if year :
+            movie_in_year = Movie.objects.filter(year=year)
         else:
-             movies_in_year = Movie.objects.filter(year__isnull=True)
-        count = movies_in_year.count()
+            movie_in_year = Movie.objects.filter(year__isnull=True)
+            year = "None"
+        count = movie_in_year.count()
         movie_counts_by_year[year] = count
 
     bar_width = 0.5
-    bar_spacing = 0.5
     bar_positions = range(len(movie_counts_by_year))
 
     plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    
     plt.title('Movies per year')
     plt.xlabel('Year')
     plt.ylabel('Number of movies')
     plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+
     plt.subplots_adjust(bottom=0.3)
+
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
@@ -53,35 +61,36 @@ def stadistics_view(request):
 
     image_png = buffer.getvalue()
     buffer.close()
-    graphic = base64.b64encode(image_png)
-    graphic = graphic.decode('utf-8-sig')
+    graphic1 = base64.b64encode(image_png)
+    graphic1 = graphic1.decode('utf-8')
 
-    return render(request, 'statistics.html', {'graphic': graphic})
+    #===========graph 2===========
 
-
-#Code to make graphic base on the amount of movies by genre
-def genres_view(request):
     matplotlib.use('Agg')
-    genre = Movie.objects.values_list('genre', flat=True).distinct().order_by('genre')
+    genres = Movie.objects.values_list('genre', flat=True).distinct()
+
     movie_counts_by_genre = {}
-    for genre in genre:
-        if genre:
-            movies_in_genre = Movie.objects.filter(genre=genre)
+    for genre in genres:
+        if genre :
+            movie_by_genre = Movie.objects.filter(genre=genre)
         else:
-             movies_in_genre = Movie.objects.filter(genre__isnull=True)
-        count = movies_in_genre.count()
+            movie_by_genre = Movie.objects.filter(genre__isnull=True)
+            genre = "None"
+        count = movie_by_genre.count()
         movie_counts_by_genre[genre] = count
 
     bar_width = 0.5
-    bar_spacing = 0.5
     bar_positions = range(len(movie_counts_by_genre))
 
     plt.bar(bar_positions, movie_counts_by_genre.values(), width=bar_width, align='center')
+    
     plt.title('Movies per genre')
     plt.xlabel('Genre')
     plt.ylabel('Number of movies')
     plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation=90)
+
     plt.subplots_adjust(bottom=0.3)
+
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
@@ -89,25 +98,7 @@ def genres_view(request):
 
     image_png = buffer.getvalue()
     buffer.close()
-    graphic = base64.b64encode(image_png)
-    graphic = graphic.decode('utf-8-sig')
+    graphic2 = base64.b64encode(image_png)
+    graphic2 = graphic2.decode('utf-8')
 
-    return render(request, 'statics.html', {'graphic': graphic})
-=======
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Movie
-
-# Create your views here.
-def home(request):
-    searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm,'movies':movies})
-
-
-def about(request):
-	return HttpResponse('<h1>Welcome to About Page</h1>')
->>>>>>> ae60163fc39d5cb5f4cd146166a880552bf3f717
+    return render(request, 'statistics.html', {'graphic1': graphic1, 'graphic2': graphic2})
